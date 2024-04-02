d7b58742d317c5b76d3eba01c2b23e6a779e13ad
didiergamassa <108032706+didiergamassa@users.noreply.github.com>
2024-04-02 09:00:33 +0200

    com1

--- backenergie.py
+++ backenergie.py
@@ -1,16 +1,25 @@
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request,send_file
 import pandas as pd
 
 app = Flask(__name__)
 
 # Simulated data (replace with actual data loading logic)
-electricity_data = pd.DataFrame({'Month': ['Jan', 'Feb', 'Mar'], 'Consumption': [100, 120, 110]})
-gas_data = pd.DataFrame({'Month': ['Jan', 'Feb', 'Mar'], 'Consumption': [50, 60, 55]})
-water_data = pd.DataFrame({'Month': ['Jan', 'Feb', 'Mar'], 'Consumption': [20, 25, 22]})
+electricity_data = pd.DataFrame({'Month': ['Jan', 'Feb', 'Mar','Avr','Mai','Juin','Juil','Aout','Sept','Oct','NoV','Dec'],'Consumption': [100, 120, 110]})
+gas_data = pd.DataFrame({'Month': ['Jan', 'Feb', 'Mar','Avr','Mai','Juin','Juil','Aout','Sept','Oct','NoV','Dec'],'Consumption': [50, 60, 55]})
+water_data = pd.DataFrame({'Month': ['Jan', 'Feb', 'Mar','Avr','Mai','Juin','Juil','Aout','Sept','Oct','NoV','Dec'],'Consumption': [20, 25, 22]})
 
 @app.route("/",methods=['GET'])
 def get_default():
     return jsonify({'result': "App Running..."})
+
+# Renvoie l'image
+@app.route('/get_image')
+def get_image():
+    # Ici, vous pouvez ajouter votre logique pour récupérer l'image à afficher
+    image_path = 'C:\\Users\\dgama\Desktop\\testback\\bird.png'  # Changer cela avec le chemin de votre image
+    # Renvoie l'image
+    return send_file(image_path, mimetype='image/png')
+
 
 @app.route('/electricity')
 def get_electricity_data():
